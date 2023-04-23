5c068bd86ab5e5d4b21182cbeb4e5e63681f5c0f
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2023-04-23 08:29:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="bg-gradient-to-b from-[#374151] to-[#111827] min-h-screen">
       <Navbar />
+      <Routes>
+        <Route path="/" Component={() => <Home />} />
+      </Routes>
     </div>
   );
 }
